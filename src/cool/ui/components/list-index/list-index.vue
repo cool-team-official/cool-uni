8b8f469861a7a4021e251d87aed1e2089bda9ea4
icssoa <2570063477@qq.com>
2020-04-24 00:07:49 +0800

    优化list-index

--- src/cool/ui/components/list-index/list-index.vue
+++ src/cool/ui/components/list-index/list-index.vue
@@ -19,18 +19,36 @@
 			enable-back-to-top
 			scroll-with-animation
 			:scroll-into-view="`index-${LI.label}`"
+			@scroll="onScroll"
 		>
 			<template v-for="(item, index) in flist">
-				<view class="cl-li__group" :key="index" :id="`index-${item.label}`">
+				<view
+					class="cl-li__group"
+					:key="index"
+					:id="`index-${item.label}`"
+				>
 					<!-- 关键字 -->
-					<view class="cl-li__header cl-sticky">
+					<view
+						:class="[
+							'cl-li__header',
+							'cl-sticky',
+							{
+								'is-active': LI.curr.label == item.label,
+							},
+						]"
+					>
 						<text>{{ item.label }}</text>
 					</view>
-					<!-- 数据单元 -->
+					<!-- 数据列表 -->
 					<view class="cl-li__container">
 						<template v-for="(item2, index2) in item.children">
-							<view class="cl-li__item" :key="index2" @tap="selectRow(item2)">
-								<text>{{ item2.label }}</text>
+							<view
+								class="cl-li__item"
+								:key="index2"
+								@tap="selectRow(item2)"
+							>
+								<cl-avatar :src="item2.avatarUrl"></cl-avatar>
+								<text>{{ item2.name }}</text>
 							</view>
 						</template>
 					</view>
@@ -40,11 +58,19 @@
 
 		<!-- 索引栏 -->
 		<view class="cl-li__bar" @touchmove.stop="barMove" @touchend="barEnd">
-			<view class="cl-li__bar-list">
+			<view
+				class="cl-li__bar-list"
+				@touchmove.stop="barMove"
+				@touchend="barEnd"
+			>
 				<template v-for="(item, index) in flist">
 					<view
-						class="cl-li__bar-block"
-						:class="[{ 'is-active': LI.curr.label == item.label }]"
+						:class="[
+							'cl-li__bar-block',
+							{
+								'is-active': LI.curr.label == item.label,
+							},
+						]"
 						:key="index"
 						:id="index"
 						@touchstart="toRow(item)"
@@ -61,36 +87,171 @@
 </template>
 
 <script>
-import CityData from '@/data/city';
-
 export default {
 	data() {
 		return {
 			LI: {
-				keyWord: '',
-				label: '',
+				keyWord: "",
+				label: "",
 				alert: false,
-				data: CityData,
+				data: [],
 				curr: {},
 				bar: {
 					top: 0,
-					itemH: 0
-				}
-			}
+					itemH: 0,
+				},
+			},
 		};
 	},
 
 	computed: {
 		flist() {
-			const match = d => d.label.includes(this.LI.keyWord);
+			const match = (d) => d.name.includes(this.LI.keyWord);
 
 			return this.LI.data
-				.filter(e => e.children && e.children.find(match))
-				.map(e => {
+				.filter((e) => e.children && e.children.find(match))
+				.map((e) => {
 					e.children = e.children.filter(match);
 					return e;
 				});
-		}
+		},
+	},
+
+	created() {
+		this.LI.data = [
+			{
+				label: "A",
+				children: [
+					{
+						avatarUrl:
+							"https://cool-comm.oss-cn-shenzhen.aliyuncs.com/show/imgs/chat/avatar/1.jpg",
+						name: "阿雪",
+					},
+					{
+						avatarUrl:
+							"https://cool-comm.oss-cn-shenzhen.aliyuncs.com/show/imgs/chat/avatar/3.jpg",
+						name: "阿良",
+					},
+					{
+						avatarUrl:
+							"https://cool-comm.oss-cn-shenzhen.aliyuncs.com/show/imgs/chat/avatar/2.jpg",
+						name: "阿绵",
+					},
+				],
+			},
+
+			{
+				label: "C",
+				children: [
+					{
+						avatarUrl:
+							"https://cool-comm.oss-cn-shenzhen.aliyuncs.com/show/imgs/chat/avatar/5.jpg",
+						name: "陈杨",
+					},
+					{
+						avatarUrl:
+							"https://cool-comm.oss-cn-shenzhen.aliyuncs.com/show/imgs/chat/avatar/6.jpg",
+						name: "陈飞",
+					},
+					{
+						avatarUrl:
+							"https://cool-comm.oss-cn-shenzhen.aliyuncs.com/show/imgs/chat/avatar/7.jpg",
+						name: "陈品如",
+					},
+					{
+						avatarUrl:
+							"https://cool-comm.oss-cn-shenzhen.aliyuncs.com/show/imgs/chat/avatar/4.jpg",
+						name: "陈逸",
+					},
+				],
+			},
+			{
+				label: "D",
+				children: [
+					{
+						avatarUrl:
+							"https://cool-comm.oss-cn-shenzhen.aliyuncs.com/show/imgs/chat/avatar/10.jpg",
+						name: "道天",
+					},
+				],
+			},
+			{
+				label: "H",
+				children: [
+					{
+						avatarUrl:
+							"https://cool-comm.oss-cn-shenzhen.aliyuncs.com/show/imgs/chat/avatar/9.jpg",
+						name: "韩琦",
+					},
+					{
+						avatarUrl:
+							"https://cool-comm.oss-cn-shenzhen.aliyuncs.com/show/imgs/chat/avatar/10.jpg",
+						name: "韩重",
+					},
+				],
+			},
+			{
+				label: "J",
+				children: [
+					{
+						avatarUrl:
+							"https://cool-comm.oss-cn-shenzhen.aliyuncs.com/show/imgs/chat/avatar/3.jpg",
+						name: "江澄",
+					},
+					{
+						avatarUrl:
+							"https://cool-comm.oss-cn-shenzhen.aliyuncs.com/show/imgs/chat/avatar/7.jpg",
+						name: "江厌离",
+					},
+				],
+			},
+			{
+				label: "S",
+				children: [
+					{
+						avatarUrl:
+							"https://cool-comm.oss-cn-shenzhen.aliyuncs.com/show/imgs/chat/avatar/2.jpg",
+						name: "苏三",
+					},
+					,
+					{
+						avatarUrl:
+							"https://cool-comm.oss-cn-shenzhen.aliyuncs.com/show/imgs/chat/avatar/4.jpg",
+						name: "沈道",
+					},
+				],
+			},
+			{
+				label: "Z",
+				children: [
+					{
+						avatarUrl:
+							"https://cool-comm.oss-cn-shenzhen.aliyuncs.com/show/imgs/chat/avatar/4.jpg",
+						name: "周蓓蓓",
+					},
+					{
+						avatarUrl:
+							"https://cool-comm.oss-cn-shenzhen.aliyuncs.com/show/imgs/chat/avatar/2.jpg",
+						name: "朱勇",
+					},
+					{
+						avatarUrl:
+							"https://cool-comm.oss-cn-shenzhen.aliyuncs.com/show/imgs/chat/avatar/3.jpg",
+						name: "朱鸣",
+					},
+					{
+						avatarUrl:
+							"https://cool-comm.oss-cn-shenzhen.aliyuncs.com/show/imgs/chat/avatar/1.jpg",
+						name: "张伟",
+					},
+					{
+						avatarUrl:
+							"https://cool-comm.oss-cn-shenzhen.aliyuncs.com/show/imgs/chat/avatar/9.jpg",
+						name: "郑金榜",
+					},
+				],
+			},
+		];
 	},
 
 	mounted() {
@@ -98,7 +259,7 @@ export default {
 
 		query
 			.select(`.cl-li__bar-list`)
-			.boundingClientRect(res => {
+			.boundingClientRect((res) => {
 				this.LI.bar.top = res.top;
 				this.LI.bar.itemH = parseInt(res.height / this.LI.data.length);
 			})
@@ -113,7 +274,9 @@ export default {
 
 		barMove(e) {
 			const max = this.LI.data.length;
-			let index = parseInt((e.touches[0].clientY - this.LI.bar.top) / this.LI.bar.itemH);
+			let index = parseInt(
+				(e.touches[0].clientY - this.LI.bar.top) / this.LI.bar.itemH
+			);
 
 			if (index >= max) {
 				index = max - 1;
@@ -133,13 +296,45 @@ export default {
 
 		selectRow(item) {
 			console.log(item);
-		}
-	}
+		},
+
+		onScroll() {
+			this.LI.data.forEach((e) => {
+				uni.createSelectorQuery()
+					.in(this)
+					.select(`#index-${e.label} .cl-li__header`)
+					.boundingClientRect((res) => {
+						if (res.top < 60) {
+							this.LI.curr = e;
+						}
+					})
+					.exec();
+			});
+		},
+	},
 };
 </script>
 
 <style lang="scss">
 page {
 	height: 100%;
+}
+
+.cl-li {
+	&__search {
+		border-bottom: 0;
+	}
+
+	&__header {
+		background-color: #fff;
+		color: #999;
+		font-weight: 500;
+	}
+
+	&__item {
+		.cl-avatar {
+			margin-right: 30rpx;
+		}
+	}
 }
 </style>
