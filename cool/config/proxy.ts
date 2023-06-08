f0c4719e1648f92d4dd324e1e28994d9ecf20953
icssoa <2570063477@qq.com>
2023-06-08 16:04:08 +0800

    优化

--- cool/config/proxy.ts
+++ cool/config/proxy.ts
@@ -1,6 +1,6 @@
 export const proxy = {
 	"/dev": {
-		target: "http://192.168.0.110:8001",
+		target: "http://127.0.0.1:8001",
 		changeOrigin: true,
 		rewrite: (path: string) => path.replace(/^\/dev/, ""),
 	},
