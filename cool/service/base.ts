60b4b7f14de16ff256c9d90f176b777df1648fca
icssoa <2570063477@qq.com>
2023-06-12 17:18:51 +0800

    优化

--- cool/service/base.ts
+++ cool/service/base.ts
@@ -60,8 +60,10 @@ export class BaseService {
 		}
 
 		// 处理地址
-		if (options.proxy === undefined || options.proxy) {
-			options.url = ns + options.url;
+		if (options.url.indexOf("http") < 0) {
+			if (options.proxy === undefined || options.proxy) {
+				options.url = ns + options.url;
+			}
 		}
 
 		// 处理参数
