215fbaea97cf86b0c8d2cb2a8f57b1743a5cd44b
神仙都没用 <615206459@qq.com>
2023-07-06 23:10:00 +0800

    优化

--- ui/components/cl-page/cl-page.vue
+++ ui/components/cl-page/cl-page.vue
@@ -1,143 +1,62 @@
 <template>
-	<view
-		class="cl-page"
-		:style="{
-			padding: parseRpx(padding),
-		}"
-	>
-		<!-- 遮罩层 -->
-		<cl-loading-mask fullscreen :loading="loader.loading" :text="loader.text"></cl-loading-mask>
-
-		<!-- 提示 -->
-		<cl-toast :ref="setRefs('toast')"></cl-toast>
-
-		<!-- 确认框 -->
-		<cl-confirm :ref="setRefs('confirm')"></cl-confirm>
-
-		<!-- 状态栏 -->
-		<!-- #ifndef MP-ALIPAY -->
-		<cl-status-bar v-if="statusBar" />
-		<!-- #endif -->
-
-		<!-- 内容插槽 -->
-		<view class="cl-page__container" :style="{ height }">
-			<slot></slot>
-		</view>
-
-		<!-- 底部安全区域 -->
-		<view class="safe-area-bottom"></view>
+	<view class="cl-sticky" :class="[{ 'is-flex': isFlex }]" :style="{ zIndex, top: stickyTop }">
+		<slot></slot>
 	</view>
-
-	<!-- 背景色 -->
-	<view
-		class="cl-page__bg"
-		:style="{
-			background,
-		}"
-	></view>
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, reactive } from "vue";
-import { useCool, useGlobal } from "/@/cool";
+import { computed, defineComponent } from "vue";
+import { useCool } from "/@/cool";
 import { parseRpx } from "/@/cool/utils";
 
 const { statusBarHeight } = uni.getSystemInfoSync();
 
 export default defineComponent({
-	name: "cl-page",
+	name: "cl-sticky",
 
 	props: {
-		fullscreen: Boolean,
-		padding: {
-			type: [Number, String],
-			default: 0,
+		zIndex: {
+			type: Number,
+			default: 401,
+		},
+		top: {
+			type: [String, Number],
+			default: "0px",
 		},
 		statusBar: {
 			type: Boolean,
 			default: true,
 		},
-		backgroundColor: String,
+		isFlex: Boolean,
 	},
 
 	setup(props) {
-		const { refs, setRefs, router } = useCool();
-		const global = useGlobal();
+		const { router } = useCool();
 
-		// 是否显示导航栏
-		const statusBar = router.info()?.isCustomNavbar ? props.statusBar : false;
+		// 吸顶高度
+		const stickyTop = computed(() => {
+			// 状态栏高度
+			const t1 = `${props.statusBar ? statusBarHeight : 0}px`;
 
-		// 内容高度
-		const height = computed(() => {
-			return props.fullscreen ? `calc(100% - ${statusBarHeight}px)` : "auto";
+			// 标题栏高度
+			// #ifdef H5
+			const t2 = "44px";
+			// #endif
+			// #ifndef H5
+			const t2 = "0px";
+			// #endif
+
+			// 自定义高度
+			const top = parseRpx(props.top);
+
+			// 计算后距离顶部高度
+			const arr = [top, ...(router.info()?.isCustomNavbar ? [t1] : [t2])];
+
+			return `calc(${arr.join(" + ")})`;
 		});
-
-		// 背景色
-		const background = computed(() => {
-			return (
-				props.backgroundColor ||
-				router.info()?.style?.backgroundColor ||
-				router.globalStyle.backgroundColor ||
-				"#fff"
-			);
-		});
-
-		// 加载器
-		const loader = reactive({
-			loading: false,
-			text: "加载中",
-		});
-
-		// 显示加载框
-		function showLoading(text: string) {
-			loader.loading = true;
-			loader.text = text;
-		}
-
-		// 隐藏加载框
-		function hideLoading() {
-			loader.loading = false;
-		}
-
-		// 提示框
-		function showToast(options: ClToast.Options) {
-			refs.toast?.open(options);
-		}
-
-		// 确认框
-		function showConfirm(options: ClConfirm.Options) {
-			refs.confirm?.open(options);
-		}
-
-		// 提示框
-		function showTips(message: string, callback?: () => void) {
-			refs.confirm?.open({
-				title: "提示",
-				message,
-				showCancelButton: false,
-				callback,
-			} as ClConfirm.Options);
-		}
-
-		// 追加方法
-		if (router.path) {
-			global.set(`cl-page__${router.path}`, {
-				showLoading,
-				hideLoading,
-				showToast,
-				showConfirm,
-				showTips,
-			});
-		}
 
 		return {
-			height,
-			background,
-			refs,
-			setRefs,
-			loader,
-			parseRpx,
-			statusBar,
+			stickyTop,
 		};
 	},
 });
