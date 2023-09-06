3ddae9076c2abacb180e75de8cc1244b2b0cda75
icssoa <615206459@qq.com>
2023-09-06 21:44:57 +0800

    优化

--- router/index.ts
+++ router/index.ts
@@ -1,6 +1,11 @@
 import { router, useStore } from "/@/cool";
 
-const ignoreToken = ["/pages/user/login", "/pages/user/captcha", "/pages/user/doc"];
+const ignoreToken = [
+	"/pages/index/home",
+	"/pages/user/login",
+	"/pages/user/captcha",
+	"/pages/user/doc",
+];
 
 router.beforeEach((to, next) => {
 	const { user } = useStore();
